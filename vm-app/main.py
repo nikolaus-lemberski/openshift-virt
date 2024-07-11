c5ef812495883fbb0903fa934d5ee57a74402395
Nikolaus Lemberski <nikolaus@lemberski.com>
2024-07-11 17:12:48 +0200

    app

--- vm-app/main.py
+++ vm-app/main.py
@@ -8,15 +8,15 @@ async def app(scope, receive, send):
 
     match scope["path"]:
         case "/":
-            text, status = "Hello from vm-app", 200
+            text, status = "Hello from vm-app\n", 200
         case "/health":
-            text, status =  "UP", 200
+            text, status =  "UP\n", 200
         case _:
-            text, status = "Page not found", 404
+            text, status = "Page not found\n", 404
 
     response_headers = [(b"content-type", b"text/plain; charset=utf-8")]
     await send({"type": "http.response.start", "status": status, "headers": response_headers})
-    await send({"type": "http.response.body", "body": f"{text.encode("UTF-8")}\n"})
+    await send({"type": "http.response.body", "body": text.encode("UTF-8")"})
 
 
 if __name__ == "__main__":
